3fc4f21f4d8089ee2475c1b16e31257fd0ddfff4
Nicky Liu <nickyliu91@gmail.com>
2018-09-18 04:18:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-  get '/newteam'
+
+  get '/newteam' do
     erb :newteam
   end
 
-  post '/team'
+  post '/team' do
     erb :team.erb
   end
 
